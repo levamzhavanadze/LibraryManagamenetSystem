3693d6e0cf8449ac77e54912a002059441102851
Levan Mzhavanadze <levamzhavanadze@gmail.com>
2024-02-20 16:16:09 +0400

    A

---
+++ LibraryManagamenetSystem/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagamenetSystem
+{
+    internal class Book
+    {
+        static int Id {  get; set; }
+
+        public string Title { get; set; } 
+
+        public string Author { get; set; }
+
+        public DateTime PublishingYear { get; set; }
+
+
+        public Book()
+        {
+            
+            Id = GenerateId();
+            
+        }
+
+        internal int GenerateId()
+        {
+            int tempId = Id;
+
+            Id = tempId;
+            tempId++;
+
+            return tempId;
+        }
+        
+
+        public override string ToString()
+        {
+            return $"BookId = {Id}; Title =  {Title}; Author = {Author}; Year of Publishing = {PublishingYear.Year} ";
+        }
+    }
+}
--- LibraryManagamenetSystem/Program.cs
+++ LibraryManagamenetSystem/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibraryManagamenetSystem;
+
+Book a = new() { Title = "wigni", Author = "levan", PublishingYear = new DateTime(2021, 1, 1) };
+Book b = new() { Title = "wigni2", Author = "levan2", PublishingYear = new DateTime(2022, 2, 2) };
+Book c = new() { Title = "wigni3", Author = "levan3", PublishingYear = new DateTime(2023, 3, 3) };
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
